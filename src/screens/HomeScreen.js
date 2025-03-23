05213cbd5693a80d5cd81b36936c5514af2a4784
hyunhu24 <140373389+hyunhu24@users.noreply.github.com>
2025-03-23 16:11:37 +0900

    branch test

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Home Screen!</Text>
+      <Text style={styles.text}>Home Screen!</Text>
     </View>
   );
 }
