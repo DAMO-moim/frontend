df7bb396c81be56d9998af4a2b046315fc4bc737
hyunhu24 <140373389+hyunhu24@users.noreply.github.com>
2025-03-24 18:42:03 +0900

    무한 스크롤 캐러셸 애니매이션 test 완료!, 로그인 후 네이게이션 바 변환 확인, 로그인 기능 간단 구현, 회원 커스텀 훅 제작..

---
+++ src/components/CategoryIcon.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+const images = {
+    Books: require("../../assets/images/category/Books.png"),
+    Music: require("../../assets/images/category/Music.png"),
+    Sports: require("../../assets/images/category/Sports.png"),
+    Car: require("../../assets/images/category/Car.png"),
+    Cook: require("../../assets/images/category/Cook.png"),
+    Dance: require("../../assets/images/category/Dance.png"),
+    Game: require("../../assets/images/category/Game.png"),
+    Languages: require("../../assets/images/category/Languages.png"),
+    MusicInstrument: require("../../assets/images/category/MusicInstrument.png"),
+    Pets: require("../../assets/images/category/Pets.png"),
+    Photo: require("../../assets/images/category/Photo.png"),
+    Society: require("../../assets/images/category/Society.png"),
+    Travel: require("../../assets/images/category/Travel.png"),
+  };
+  export const CategoryIcon = ({ imageName, onPress, style }) => {
+    return (
+      <TouchableOpacity style={[style.container, style]} onPress={onPress}>
+        <Image source={images[imageName]} style={style.image} />
+      </TouchableOpacity>
+    );
+  };
---
+++ src/screens/loginAfter/ProfileScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { commonStyles } from '../../constants/styles';
+
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={commonStyles.container}>
+      <Text style={commonStyles.text}>Profile Screen</Text>
+    </View>
+  );
+}
+
+export default ProfileScreen;
