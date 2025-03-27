ab299f2a8472ffa6c3934352118f61d367b39fff
hyunhu24 <140373389+hyunhu24@users.noreply.github.com>
2025-03-27 20:30:48 +0900

    토스트 메세지 컴포넌트 제작(스타일 적용 X), 체크박스 컴포넌트 제작

--- src/api/queries/categoryService.js
+++ src/api/queries/categoryService.js
@@ -1,13 +1,12 @@
 import { instance } from '../axiosInstance';
 
-export const fetchCategories = async (memberId, token) => {
+export const fetchCategories = async () => {
   try {
-    const response = await instance.get(`/members/categories`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    return response.data.data; // 카테고리 데이터 반환
+    const response = await instance.get(`/members/categories`);
+    console.log(response.data.data);
+    return response.data.data;
   } catch (error) {
-    console.error('Error fetching categories:', error);
+    console.error('Error fetching categories:', error.response?.data || error.message);
     return [];
   }
 };
